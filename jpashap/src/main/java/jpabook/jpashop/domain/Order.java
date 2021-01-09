ee83420ff6c21dadc346a32b0e873dce4775200a
장서현 <rxforp@naver.com>
2021-01-10 00:22:43 +0900

    entity maping

---
+++ jpashap/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name="ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private Member member;
+
+    private LocalDateTime orderDate;    //ORDER_DATE, order_date
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+}
