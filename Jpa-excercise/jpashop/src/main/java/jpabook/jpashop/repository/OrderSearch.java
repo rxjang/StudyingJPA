40363dbdf1d4d5ab98a2e3d3ed9fe05ae0f162cc
장서현 <rxforp@naver.com>
2021-07-16 07:59:08 +0900

    Search and cancel order

--- Jpa-excercise/jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ Jpa-excercise/jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -1,7 +1,10 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class OrderSearch {
 
     private String memberName; //회원 이름
