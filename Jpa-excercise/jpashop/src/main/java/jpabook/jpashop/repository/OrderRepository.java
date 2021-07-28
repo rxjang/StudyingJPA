0f1146b3343646130e8c0de7b76c62763a360f4f
장서현 <rxforp@naver.com>
2021-07-28 21:55:10 +0900

    resolve paging in xToMany relation

--- Jpa-excercise/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ Jpa-excercise/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -66,6 +66,18 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+//        return em.createQuery("select o FROM Order o " +
+//                "JOIN FETCH o.member m " +
+//                "JOIN FETCH o.delivery d", Order.class)
+//                .setFirstResult(offset)
+//                .setMaxResults(limit)
+//                .getResultList();
+        return em.createQuery("select o FROM Order o ", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
 
     public List<Order> findAllWithItem() {
         return em.createQuery(
@@ -75,4 +87,25 @@ public class OrderRepository {
                         "JOIN FETCH oi.item i", Order.class)
                 .getResultList();
     }
+
+    public List<Order> findAllWithItems() {
+        return em.createQuery(
+                "SELECT o FROM Order o " +
+                        "JOIN FETCH o.member m " +
+                        "JOIN FETCH o.delivery d " +
+                        "JOIN FETCH o.orderItems oi " +
+                        "JOIN FETCH oi.item i", Order.class)
+                .setFirstResult(1)
+                .setMaxResults(100)
+                .getResultList();
+    }
 }
+
+
+
+
+
+
+
+
+
